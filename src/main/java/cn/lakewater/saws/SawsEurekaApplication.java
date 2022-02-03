a6f3603fa62e7780b982e084b6bff01a58bb8d98
squirrelRao <hqraop@163.com>
2022-02-03 15:47:49 +0800

    first commit saws erueka

--- src/main/java/cn/lakewater/saws/SawsEurekaApplication.java
+++ src/main/java/cn/lakewater/saws/SawsEurekaApplication.java
@@ -1,2 +1,16 @@
-package cn.lakewater.saws;public class SawsEurekaApplication {
+package cn.lakewater.saws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableEurekaServer
+@ComponentScan
+public class SawsEurekaApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SawsEurekaApplication.class);
+    }
 }
--- src/main/java/cn/lakewater/saws/controller/MainController.java
+++ src/main/java/cn/lakewater/saws/controller/MainController.java
@@ -1,2 +1,17 @@
-package cn.lakewater.saws.controller;public class MainController {
+package cn.lakewater.saws.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class MainController {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        log.info("hello");
+        return "hello eureka!";
+    }
+
 }
